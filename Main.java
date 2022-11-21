6a1c8fb436a33e6dfc90c0887f1c9138390dd095
Kevin Martínez <80003508+Transa1@users.noreply.github.com>
2022-11-20 17:06:29 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,17 +5,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Persona p = new Persona();
         String opcion;
+        //if(!configuracion.exist()){
+        //}
         boolean rep = true;
 
         do {
-            opcion = JOptionPane.showInputDialog("Ingrese la priemra letra de la opcion\nAgregar personas\nSalir");
+            opcion = JOptionPane.showInputDialog("Ingrese la primera letra de la opcion\nAgregar personas\nVer contactos\nSalir");
+            opcion = opcion.toUpperCase();
             if (opcion.compareTo("A") == 0) {
                 p.agregarPersona();
                 p.guardar();
             }
-            if (opcion.compareTo("S") == 0) {
+            else if (opcion.compareTo("S") == 0) {
                 rep = false;
+            }
+            else if(opcion.compareTo("V") == 0){
+                p.verContactos();
+            }
+            else{
+                JOptionPane.showMessageDialog(null,"Ingrese un valor valido");
             }
         }while (rep);
     }
 }
+
